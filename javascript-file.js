2ffd6ff1212ce1bd8c0fae176e96f2794cb46dcb
silverjin <50380898+silverjin@users.noreply.github.com>
2021-01-22 17:27:32 +0100

    Add files via upload

--- javascript-file.js
+++ javascript-file.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
     $(this).next().find(".panel").slideToggle("slow");
   });
 });
+
+jQuery(document).ready(function() {
+
+    // add class on hover
+    jQuery('.panel, .flip').hover(function() {
+      jQuery(this).addClass('panel, .flip:hover');
+    });
+
+    // remove class on transition end
+    jQuery('.panel, .flip').bind('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+      jQuery(this).delay(1000).queue(function() {
+      jQuery(this).removeClass('panel, .flip:hover');
+    });
+    });
+});
